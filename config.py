008bcafca9dd587a59a70a8d88a307c048d325b2
Dustin L <r.dustin.lane@gmail.com>
2023-05-21 20:56:41 -0700

    Correct default configs

--- config.py
+++ config.py
@@ -15,16 +15,16 @@ config = {
     # App Configuration
     "device_name": "CO2_SCD41",
     "light_sleep_sec": const(5),
-    "deep_sleep_sec": const(10),
-    "upload_rate_sec": const(30),
+    "deep_sleep_sec": const(120),
+    "upload_rate_sec": const(120),
     "receive_rate_sec": const(120),
     "receive_window_sec": const(0.5),
     "time_sync_rate_sec": const(600),
-    "send_discovery_rate_sec": const(20),
+    "send_discovery_rate_sec": const(600),
     "display_refresh_rate_sec": const(120),
     "ambient_pressure": const(1000),
     "temp_offset_c": const(0.5),
-    "force_deep_sleep": True,
+    "force_deep_sleep": False,
     "logging_level": logging.DEBUG,
 
     # Display Configuration
@@ -49,7 +49,7 @@ config = {
     "connect_retries": const(5),
     "recv_timeout_sec": const(10),
 
-    #Espnow configuration parameters
+    # Espnow configuration parameters
     "epn_peer_mac": b'|\xdf\xa1\xf2\xa2\xd0',
     "epn_channel": const(1)
 }
